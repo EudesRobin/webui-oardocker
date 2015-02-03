c425c097eade590b529a10ad3d709e86d0c53dff
root <root@725d89764bb4>
2015-02-03 13:38:19 +0000

    first json request ok

--- lab.php
+++ lab.php
@@ -1,3 +1,21 @@
 <?php
-echo exec('ls');
+	phpinfo();
+
+$url="http://localhost/oarapi/resources.json";
+//  Initiate curl
+$ch = curl_init();
+// Disable SSL verification
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false1111);
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$url);
+// Execute
+$result=curl_exec($ch);
+// Closing
+curl_close($ch);
+
+// Will dump a beauty json :3
+var_dump(json_decode($result, true));
+
 ?>
