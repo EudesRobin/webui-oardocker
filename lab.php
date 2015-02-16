b6f7163c9d07a52d7459bd87651ffa53ef4b646e
root <root@725d89764bb4>
2015-02-16 17:04:52 +0000

    state+progress barre

--- lab.php
+++ lab.php
@@ -1,5 +1,4 @@
 <?php
-
 include 'header.php';
 gen_header("General view");
 
@@ -27,14 +26,14 @@ curl_close($ch);
 $json_array = json_decode($result,true);
 
 
-echo '<div class="jumbotron">
-        <h1>Ensemble des ressources</h1>
-        <p>Diverses infos à compléter...</p>
-      </div>';
 
-echo '<div class="page-header">
-        <h1>Resumé</h1>
-      </div>';
+echo '<div class="container theme-showcase" role ="main">
+<div class="jumbotron">
+<h1>Ensemble des ressources</h1>
+<p> EN cours de complétion</p>
+</div>
+<div class="page-header">
+        <h1>Resumé</h1></div>';
 
 echo '      <div class="row">
         <div class="col-md-6">
@@ -43,18 +42,41 @@ echo '      <div class="row">
               <tr>
                 <th>#</th>
                 <th>NET_ADDR</th>
+		<th>STATE</th>
               </tr>
             </thead>
             <tbody>';
 		//parcours json_array
+		$taille = count($json_array['items']);
+
+		$alive = 0;
+		$absent = 0;
+		$dead =0;
+
 		foreach ($json_array['items'] as $key => $value) {
-		    echo '<tr><td>'.$value['id'].'</td>'.'<td>'.$value['network_address'].'</td></tr>';
+		    echo '<tr><td>'.$value['id'].'</td>'.'<td>'.$value['network_address'].'</td>';
+		    if(!strpos($value['state'],'Alive')){
+			$alive++;
+			echo '<td><button type="button" class="btn btn-lg btn-success">'.$value['state'].'</button></td></tr>';
+		   }else if(!strpos($value['state'],'Absent')){
+			$absent++;
+			echo '<td><button type="button" class="btn btn-lg btn-warning">'.$value['state'].'</button></td></tr>';
+		  }else{
+			$dead++;
+			echo '<td><button type="button" class="btn btn-lg btn-danger">'.$value['state'].'</button></td></tr>';
+		  }
 		}
               echo '
             </tbody>
           </table>
-        </div>
-</body></html>';
+        </div></div>';
+     // Barre de  "vie" des nodes...
+      echo '<div class="page-header"><h1>PROG BRRE</h1></div><div class="progress">
+        <div class="progress-bar progress-bar-success" style="width: '.($alive/$taille*100).'%"><span class="sr-only">Complete (success)</span></div>
+        <div class="progress-bar progress-bar-warning" style="width: '.($absent/$taille*100).'%"><span class="sr-only">Complete (warning)</span></div>
+        <div class="progress-bar progress-bar-danger" style="width: '.($dead/$taille*100).'%"><span class="sr-only">Complete (danger)</span></div>
+	</div></div></div>';
+	echo '</body></html>';
 
 
 ?>
--- nav.php
+++ nav.php
@@ -4,30 +4,17 @@ echo    '<!-- Fixed navbar -->
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Bootstrap theme</a>
+          <a class="navbar-brand" href="#">WEBUI</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
+            <li class="active"><a href="lab.php">Home</a></li>
+            <li><a href="../monika/">Monika</a></li>
+            <li><a href="../drawgantt-svg/">Graphe Gant</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
