5228793b333bf425f1268662437d5dbedb5c0c05
Eudes Robin <eudes.robin@gmail.com>
2015-02-23 00:16:29 +0100

    fix footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php
 echo 
-	'<footer class="navbar navbar-default navbar-fixed-bottom">
+	'<footer class="navbar navbar-default navbar-bottom">
 		<div class="container">
 			<p class="text-muted"> Projet 2014-2015 Polytech\' Grenoble - WEBUI OAR-DOCKER - Eudes Robin & Rossi Ombeline</p>
 		</div>
