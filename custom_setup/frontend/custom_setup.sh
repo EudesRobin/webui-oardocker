895873d8f162c9adf4a335b44e9515baa5e41c42
Eudes Robin <eudes.robin@gmail.com>
2015-02-23 13:44:18 +0100

    fix custom setup + add auth

--- custom_setup/frontend/custom_setup.sh
+++ custom_setup/frontend/custom_setup.sh
@@ -7,9 +7,9 @@ apt-get -y install git-core php5-curl
 service apache2 restart
 
 echo " Node.js setup"
-wget -O /tmp/node-v0.12.0-linux-x64.tar.gz http://nodejs.org/dist/v0.12.0/node-v0.12.0-linux-x64.tar.gz;
+wget -O /tmp/node-v0.12.0-linux-x64.tar.gz http://nodejs.org/dist/v0.12.0/node-v0.12.0-linux-x64.tar.gz
 cd /usr/local && tar --strip-components 1 -xzf /tmp/node-v0.12.0-linux-x64.tar.gz
-rm /tmp/node-v0.12.0-linux-x64.tar.gz;
+rm /tmp/node-v0.12.0-linux-x64.tar.gz
 
 echo "Boostrap setup"
 cd /var/www
@@ -20,5 +20,9 @@ npm install
 grunt dist
 
 # Finally, we clone the webui :)
-cd /var/www;
-sudo git clone https://github.com/EudesRobin/webui-oardocker.git;
+cd /var/www
+git clone https://github.com/EudesRobin/webui-oardocker.git
+
+echo "Creation compte apache"
+echo " username : docker "
+htpasswd -c /etc/apache2/.htpasswd docker
