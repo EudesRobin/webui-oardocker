2dd65463905317df9ec6a706b29f39fc64421967
Eudes Robin <eudes.robin@gmail.com>
2015-03-02 13:24:13 +0100

    boostrap fix

--- custom_setup/frontend/custom_setup.sh
+++ custom_setup/frontend/custom_setup.sh
@@ -5,18 +5,18 @@ set -e
 echo "Git & curl setup"
 apt-get -y install git-core php5-curl
 
-echo " Node.js setup"
-wget -O /tmp/node-v0.10.1-linux-x64.tar.gz http://nodejs.org/dist/v0.10.1/node-v0.10.1-linux-x64.tar.gz
-cd /usr/local && tar --strip-components 1 -xzf /tmp/node-v0.10.1-linux-x64.tar.gz
-rm /tmp/node-v0.10.1-linux-x64.tar.gz
+#echo " Node.js setup"
+#wget -O /tmp/node-v0.10.1-linux-x64.tar.gz http://nodejs.org/dist/v0.10.1/node-v0.10.1-linux-x64.tar.gz
+#cd /usr/local && tar --strip-components 1 -xzf /tmp/node-v0.10.1-linux-x64.tar.gz
+#rm /tmp/node-v0.10.1-linux-x64.tar.gz
 
-echo "Boostrap setup"
-cd /var/www
-npm install bootstrap
-npm install -g grunt-cli
-cd node_modules/bootstrap/
-npm install
-grunt dist
+#echo "Boostrap setup"
+#cd /var/www
+#npm install bootstrap
+#npm install -g grunt-cli
+#cd node_modules/bootstrap/
+#npm install
+#grunt dist
 
 echo " Clone webui"
 cd /var/www
