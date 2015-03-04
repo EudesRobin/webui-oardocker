1e9502677cc1b8287610ca9adf2a486e4376b57e
Carnelune <rossi.ombeline@gmail.com>
2015-03-04 16:04:01 +0100

    fix deco

--- header.php
+++ header.php
@@ -42,11 +42,11 @@ echo
           <ul class="nav navbar-nav">
             <li><a href="index.php">General view</a></li>
             <li><a href="../monika/">Monika</a></li>
-            <li><a href="../drawgantt-svg/">Graphe Gant</a></li>'
+            <li><a href="../drawgantt-svg/">Graphe Gant</a></li>';
 		if ($_SESSION['login'] == 'docker') {
-			echo '<li><a href="authentification.php">Se déconnecter (docker)</a></li>'
+			echo '<li><a href="authentification.php">Se déconnecter (docker)</a></li>';
 		} else {
-	    echo '<li><a href="authentification.php">Authentification</a></li>'
+	    echo '<li><a href="authentification.php">Authentification</a></li>';
 		}
          echo '</ul>
         </div><!--/.nav-collapse -->
