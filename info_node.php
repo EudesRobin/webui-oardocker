ddaaa7c98772eb612fa867c4f3b3003a8d22db75
Eudes Robin <eudes.robin@gmail.com>
2015-03-16 16:34:01 +0100

    refonte gen info_node.php

--- info_node.php
+++ info_node.php
@@ -1,21 +1,42 @@
 <?php
 include 'header.php';
-include './action/json_functions.php';
-
-gen_header("General view");
-echo '
-		<script type="text/javascript" charset="utf-8">
+gen_header("Info Node");
+session_start(); ?>
+			<script type="text/javascript" charset="utf-8">
 			$(document).ready(function() {
-				$(\'#tab\').dataTable({
-					"order": [[ 0, "asc" ]]
+				$('#tab').dataTable({
+					"ajax": {
+						"url" : "http://localhost:48080/oarapi/resources/full.json",
+						"dataSrc":"items"
+					},
+					"columns": [
+					{ "data": "host","title": "Hostname",
+						"render": function ( data, type, full, meta ) {
+							return '<a href="/webui-oardocker/info_rsc.php?network_id='+data+'"><button type="button" class="btn btn-lg btn-default">'+data+'</button></a>';
+					    }
+					},
+					{ "data" : "cpu","title": "CPU ID"},
+					{ "data" : "core","title": "Core ID"},
+					{ "data" : "state", "title": "State",
+						"render": function ( data, type, full, meta ) {
+							if(data=='Alive'){
+								return '<button type="button" class="btn btn-lg btn-success">'+data+'</button>';
+							}else if(data=='Absent'){
+								return '<button type="button" class="btn btn-lg btn-warning">'+data+'</button>';
+							}
+							return '<button type="button" class="btn btn-lg btn-danger">'+data+'</button>';
+					    }
+					},
+					{"data":"core", "title": "Action",
+						"render": function ( data, type, full, meta ){
+							return '<a href="/webui-oardocker/job.php?core='+data+'"><button type="button" class="btn btn-lg btn-success">Send a job to this core</button></a>';
+						}	
+					}
+					],
+					"order": [[ 0, "asc" ]],
 				});
 			} );
-		</script>';
-
-$url = 'http://localhost/oarapi/resources/full.json';
-$json_array  = json_request_simple_url($url);
-
-echo '
+		</script>
 <div class="container theme-showcase" role ="main">
 	<div class="jumbotron">
 		<h1>General view of the nodes</h1>
@@ -27,53 +48,9 @@ echo '
 		<div class="col-md-6">
 		<!--begining table -->
 			<table id="tab" class="table table-striped">
-				<thead>
-					<tr>
-						<th>Hostname</th>
-						<th>CPU ID</th>
-						<th>CORE ID</th>
-						<th>STATE</th>
-						<th>OTHERS</th>
-					</tr>
-				</thead>
-				<tbody>';
-$alive = 0;
-$absent = 0;
-$dead =0;
-
-foreach ($json_array['items'] as $key => $value) {
-echo '
-					<tr><td>'.$value['network_address'].'</td><td>'.$value['cpu'].'</td><td>'.$value['core'];
-
-	if(strcmp($value['state'],"Alive")==0){
-		$alive++;
-			echo '<td><button type="button" class="btn btn-lg btn-success">'.$value['state'].'</button></td>
-				<td><a href="info_rsc.php?network_id='.$value['network_address'].'"><button type="button" class="btn btn-lg btn-success">Details & submit job</button></a></td></tr>';
-	}else if(strcmp($value['state'],"Absent")==0){
-		$absent++;
-		echo '<td><button type="button" class="btn btn-lg btn-warning">'.$value['state'].'</button></td>
-				<td><a href="info_rsc.php?network_id='.$value['network_address'].'"><button type="button" class="btn btn-lg btn-warning">Details & submit job</button></a></td></tr>';
-	}else{
-		$dead++;
-		echo '<td><button type="button" class="btn btn-lg btn-danger">'.$value['state'].'</button></td>
-				<td><a href="info_rsc.php?network_id='.$value['network_address'].'"><button type="button" class="btn btn-lg btn-danger">Details & submit job</button></a></td></tr>';
-	}
-}
-echo '
-				</tbody>
 			</table>
-		</div><!--end col-md-6 -->
-	</div><!--row -->
-	<div class="page-header">
-		<h1>General Availability</h1>
+		</div>
 	</div>
-	<div class="progress">
-		<div class="progress-bar progress-bar-success" style="width: '.($alive/$json_array['total']*100).'%"><span class="sr-only">Complete (success)</span></div>
-	        <div class="progress-bar progress-bar-warning" style="width: '.($absent/$json_array['total']*100).'%"><span class="sr-only">Complete (warning)</span></div>
-	        <div class="progress-bar progress-bar-danger" style="width: '.($dead/$json_array['total']*100).'%"><span class="sr-only">Complete (danger)</span></div>
-	</div>
-</div><!-- end container theme-showcase -->
-';
+</div>
+<?php include 'footer.php'; ?>
 
-include 'footer.php';
-?>
--- test/datatable.php
+++ test/datatable.php
@@ -1,37 +1,7 @@
-<html lang="fr">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Oar-docker Webui">
-	<meta name="author" content="Eudes Robin - Rossi Ombeline">
-	<link rel="icon" href="/webui-oardocker/favicon.ico">
-	<title>test</title>
-	<!-- Bootstrap core CSS -->
-	<link href="/webui-oardocker/bootstrap-3.3.2/css/bootstrap.min.css" rel="stylesheet">
-	<!-- Bootstrap theme -->
-	<link href="/webui-oardocker/bootstrap-3.3.2/css/bootstrap-theme.min.css" rel="stylesheet">
-	<!-- Datatable bootstrap integration -->
-	<link rel="stylesheet" type="text/css" href="/webui-oardocker/bootstrap-3.3.2/plugins/datatables-1.10/css/dataTables.bootstrap.css">
-
-	<!-- jquery ... -->
-	<script type="text/javascript" language="javascript" src="/webui-oardocker/bootstrap-3.3.2/js/jquery-2.1.3.min.js"></script>
-	<!-- Bootstrap js -->
-	<script src="/webui-oardocker/bootstrap-3.3.2/js/bootstrap.min.js"></script>
-	<!-- jquery datatables plugins -->
-	<script type="text/javascript" language="javascript" src="/webui-oardocker/bootstrap-3.3.2/plugins/datatables-1.10/js/jquery.dataTables.min.js"></script>
-	<!-- datatable bootstrap js-->
-	<script type="text/javascript" language="javascript" src="/webui-oardocker/bootstrap-3.3.2/plugins/datatables-1.10/js/dataTables.bootstrap.js"></script>
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
-	<!-- check https://datatables.net/reference/option/columns.data -->
-</head>
-<body>
+<?php
+include '../header.php';
+gen_header("Test");
+session_start(); ?>
 			<script type="text/javascript" charset="utf-8">
 			$(document).ready(function() {
 				$('#tab').dataTable({
@@ -40,11 +10,30 @@
 						"dataSrc":"items"
 					},
 					"columns": [
-					{ "data": "host"},
-					{ "data" : "core"},
-					{ "data" : "cpu"},
-					{ "data" : "state"}
-					]
+					{ "data": "host","title": "Hostname",
+						"render": function ( data, type, full, meta ) {
+							return '<a href="/webui-oardocker/info_rsc.php?network_id='+data+'"><button type="button" class="btn btn-lg btn-default">'+data+'</button></a>';
+					    }
+					},
+					{ "data" : "cpu","title": "CPU ID"},
+					{ "data" : "core","title": "Core ID"},
+					{ "data" : "state", "title": "State",
+						"render": function ( data, type, full, meta ) {
+							if(data=='Alive'){
+								return '<button type="button" class="btn btn-lg btn-success">'+data+'</button>';
+							}else if(data=='Absent'){
+								return '<button type="button" class="btn btn-lg btn-warning">'+data+'</button>';
+							}
+							return '<button type="button" class="btn btn-lg btn-danger">'+data+'</button>';
+					    }
+					},
+					{"data":"core", "title": "Action",
+						"render": function ( data, type, full, meta ){
+							return '<a href="/webui-oardocker/job.php?core='+data+'"><button type="button" class="btn btn-lg btn-success">Send a job to this core</button></a>';
+						}	
+					}
+					],
+					"order": [[ 0, "asc" ]],
 				});
 			} );
 		</script>
@@ -59,19 +48,8 @@
 		<div class="col-md-6">
 		<!--begining table -->
 			<table id="tab" class="table table-striped">
-
-			<thead>
-				<tr>
-					<th>Hostname</th>
-					<th>CPU ID</th>
-					<th>CORE ID</th>
-					<th>STATE</th>
-				</tr>
-			</thead>
-
-
+			</table>
 		</div>
 	</div>
-</body>
-</html>
+<?php include '../footer.php'; ?>
 
