0977eee409f873bfdc24a5442d585289548ac9c4
Carnelune <rossi.ombeline@gmail.com>
2015-03-23 14:47:30 +0100

    indications création ressource

--- create.php
+++ create.php
@@ -1,13 +1,13 @@
 <?php
 include 'header.php';
 
-gen_header("Create resources");
+gen_header("Add resources");
 echo '<div class="container theme-showcase" role ="main">
 			<div class="jumbotron">
-			<h1>Create resources</h1>
+			<h1>Add a resource</h1>
 			</div>
 			<div class="page-header">
-			    <h1>Create a resource</h1>
+			    <h1>Add a resource</h1>
 			    <p>For now, you can only add a cpu/core to an existing host.
 			    cpu, mem are integers.
 			    
@@ -28,14 +28,15 @@ echo '<div class="container theme-showcase" role ="main">
 		              <tr>
 		                <th></th>
 		                <th>Parameters</th>
+				<th></th>
 		              </tr>
 		            </thead>
 	            	<tbody>
 
-					 <tr><td>network_address (hostname) </td><td><input type="text" name="hostname" size="50" value="an existing hostname for now !"/></td></tr>
-					 <tr><td>CPU ID</td><td><input type="text" name="cpu" size="50" value="existing or new cpu id on this host"/></td></tr>
-					 <tr><td>MEM of this core</td><td><input type="text" name="mem" size="50" value=""/></td></tr>
-					 <tr><td>Others properties</td><td><input type="text" name="properties" size="50" value=""/></td></tr>
+					 <tr><td>network_address (hostname) </td><td><input type="text" name="hostname" size="50" value=""/></td><td><img src="Help.png" title ="for exemple: node1"></td></tr>
+					 <tr><td>CPU ID</td><td><input type="text" name="cpu" size="50" value=""/></td><td><img src="Help.png" title ="for exemple: 1, could be a new cpu id"></td></tr>
+					 <tr><td>MEM of this core</td><td><input type="text" name="mem" size="50" value=""/></td><td><img src="Help.png" title ="for exemple: 4"></td></tr>
+					 <tr><td>Others properties</td><td><input type="text" name="properties" size="50" value=""/></td><td></td></tr>
 					 <tr><td></td><td><input type="submit" value="OK"></td></tr>
 
 					</tbody>
