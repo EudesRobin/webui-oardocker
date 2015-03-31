2a7964790688b5eae80ccec5a767f7f7f14549ea
Carnelune <rossi.ombeline@gmail.com>
2015-03-31 17:20:39 +0200

    debug du display lorsqu'on ne selectionne pas de core

--- errors.php
+++ errors.php
@@ -1,36 +1,7 @@
 <?php
 
-	if(isset($_GET['pb'])){
-		if(strcmp($_GET['pb'],"nolog")==0){
-			echo '<div class="alert alert-danger" role="alert">
-				<strong>You must be logged to do this action</strong> (docker user to submit/delete a job, oar user to create/delete a resource)
-			</div>';
-			//header("refresh:3;/webui-oardocker/index.php");
-		}else if(strcmp($_GET['pb'],"wrongauth")==0){
-  	      		echo '<div class="alert alert-danger" role="alert">
-        			<strong>Invalid Login/password</strong>
-      			</div>';
-      			//header("refresh:3;/webui-oardocker/auth/login.php");
-  		}else if(strcmp($_GET['pb'],"rsc_cmd")==0){
-  		  	echo '<div class="alert alert-danger" role="alert">
-  			      <strong>Missing parameter(s).</strong> A job submission must target a resource and execute a command.
-      			</div>';
-      			//header("refresh:3;/webui-oardocker/job.php?core={$_SESSION['job.php']['GET_BACKUP']['core']}");
-  		}else if(strcmp($_GET['pb'],"wronguser")==0){
-          		echo '<div class="alert alert-danger" role="alert">
-        			<strong>Wrong user</strong> for this action
-      			</div>';
-      		//	header("refresh:3;/webui-oardocker/index.php");
-    		}else if(strcmp($_GET['pb'],"ressource_inex")==0){
-			echo '<div class="alert alert-danger" role="alert">
-      	  			The selected core <strong>does not exist.</strong>
-      			</div>';
-	 		//header("refresh:3;/webui-oardocker/job.php");
-		}else if(strcmp($_GET['pb'],"bad_res")==0){
-			echo '<div class="alert alert-danger" role="alert">
-      	  			The reservation date <strong>is not valid.</strong>
-      			</div>';
-	 		//header("refresh:3;/webui-oardocker/job.php");
+/*	if(isset($_GET['pb'])){
+
 		}else if(strcmp($_GET['pb'],"nocore")==0){
           		echo '<div class="alert alert-danger" role="alert">
         			<strong>No core selected</strong> so I can display nothing..
@@ -51,21 +22,42 @@
     			            The host targeted was not found , resource creation aborted.
     		        </div>';
     			  //header("refresh:3;/webui-oardocker/index.php");
-    		}else if(strcmp($_GET['pb'],"not_unique")==0){
-    			  echo '<div class="alert alert-danger" role="alert">
-    			            Core ID already existant, resource creation aborted.
-    		        </div>';
-    			  //header("refresh:3;/webui-oardocker/index.php");
-    		}else if(strcmp($_GET['pb'],"not_log")==0){
-    			  echo '<div class="alert alert-danger" role="alert">
-    			            You must be logged to do that.
-    		        </div>';
-    			  //header("refresh:3;/webui-oardocker/index.php");
-    		}else{
-			echo '<div class="alert alert-danger" role="alert">
-        			<strong>Unknow error</strong> Somehow, somewhere, something went terribly wrong :\'(
-      			</div>';
-		}		
 
-}
+		}*/
+		switch($_GET['pb']){
+			case "wronguser":
+				echo '<div class="alert alert-danger" role="alert">
+        				<strong>Wrong user</strong> for this action
+      				</div>';
+      				break;
+      			case "nocore":
+      				echo '<div class="alert alert-danger" role="alert">
+      					<strong>No core selected</strong> so I can display nothing...
+      				</div>';
+      				break;
+      			case "wrong_param":
+      				echo '<div class="alert alert-danger" role="alert">
+      					You entered an invalid parameter.
+      				</div>';
+      				break;
+      			case "error_create":
+      				echo '<div class="alert alert-danger" role="alert">
+      					You entered an invalid parameter, resource not created.
+      				</div>';
+      				break;
+      			case "no_host":
+      				echo '<div class="alert alert-danger" role="alert">
+      					The host targeted was not found , resource creation aborted.
+      				</div>';
+      				break;
+      			case "not_log":
+      				echo '<div class="alert alert-danger" role="alert">
+      					You must be logged to do that.
+      				</div>';
+      				break;
+      			default:
+      				echo '<div class="alert alert-danger" role="alert">
+        				<strong>Unknow error</strong> Somehow, somewhere, something went terribly wrong :\'(
+      				</div>';
+      		}		
 ?>
--- info_rsc.php
+++ info_rsc.php
@@ -2,17 +2,17 @@
 	include 'header.php';
 	include './action/json_functions.php';
 
-  if(!empty($_GET['core'])){
-    $title = 'Details of core'.$_GET['core'];
-    $url = 'http://localhost/oarapi/resources/'.$_GET['core'].'.json';
-    $json_grp  = json_request_simple_url($url);
-    gen_header($title);
+	if(!empty($_GET['core'])){
+    		$title = 'Details of core'.$_GET['core'];
+    		$url = 'http://localhost/oarapi/resources/'.$_GET['core'].'.json';
+    		$json_grp  = json_request_simple_url($url);
+    		gen_header($title);
 ?>
 <script type="text/javascript" charset="utf-8">
 $(document).ready(function() {
-    $('#tab').DataTable({
-      "iDisplayLength": 25
-    });
+    	$('#tab').DataTable({
+      		"iDisplayLength": 25
+    	});
 } );
 </script>
 <?php
@@ -23,69 +23,70 @@ echo '
 		<h1>Details</h1>
 	</div><!--end jumbotron -->
 	<div class="page-header">
-	<h1>Properties</h1>
+		<h1>Properties</h1>
 	</div>
 	<div class="row">
 		<div class="col-md-6">
-		<!--begining table -->
-    <a href="/webui-oardocker/job.php?core='.$_GET['core'].'"><button type="button" class="btn btn-lg btn-primary">Send a job to this core</button></a>
-		<br />
-		<br />
-		<br />
-		<table id="tab" class="table table-striped">
-			<thead>
-				<tr>
-					<th>Property</th>
-          <th>Value</th>
-        </tr>
-      </thead>';
+			<!--begining table -->
+			<a href="/webui-oardocker/job.php?core='.$_GET['core'].'"><button type="button" class="btn btn-lg btn-primary">Send a job to this core</button></a>
+			<br />
+			<br />
+			<br />
+			<table id="tab" class="table table-striped">
+				<thead>
+					<tr>
+						<th>Property</th>
+        					  <th>Value</th>
+        				</tr>
+      				</thead>';
 
-  echo '<tr><td>scheduler_priority</td><td>'.$json_grp['scheduler_priority'].'</td></tr>';
-  echo '<tr><td>finaud_decision</td><td>'.$json_grp['finaud_decision'].'</td></tr>';
-  echo '<tr><td>core</td><td>'.$json_grp['core'].'</td></tr>';
-  echo '<tr><td>deploy</td><td>'.$json_grp['deploy'].'</td></tr>';
-  echo '<tr><td>besteffort</td><td>'.$json_grp['besteffort'].'</td></tr>';
-  echo '<tr><td>cpuset</td><td>'.$json_grp['cpuset'].'</td></tr>';
-  echo '<tr><td>last_job_date</td><td>'.$json_grp['last_job_date'].'</td></tr>';
-  echo '<tr><td>desktop_computing</td><td>'.$json_grp['desktop_computing'].'</td></tr>';
-  echo '<tr><td>drain</td><td>'.$json_grp['drain'].'</td></tr>';
-  echo '<tr><td>state</td><td>'.$json_grp['state'].'</td></tr>';
-  echo '<tr><td>available_upto</td>';
-  switch($json_grp['available_upto']){
-    case 0:
-      echo '<td>'.'0'.'</td></tr>';
-      break;
-    case 1:
-      echo '<td>'.'1'.'</td></tr>';
-      break;
-    case 2147483647:
-      echo '<td>'.'Forever waking-up'.'</td></tr>';
-      break;
-    case 2147483646:
-      echo '<td>'.'Forever'.'</td></tr>';
-      break;
-  }
-  echo '<tr><td>id</td><td>'.$json_grp['id'].'</td></tr>';
-  echo '<tr><td>cpu</td><td>'.$json_grp['cpu'].'</td></tr>';
-  echo '<tr><td>api_timestamp</td><td>'.date('d/m/y',$json_grp['api_timestamp']).'</td></tr>';
-  echo '<tr><td>expiry_date</td><td>'.$json_grp['expiry_date'].'</td></tr>';
-  echo '<tr><td>host</td><td>'.$json_grp['host'].'</td></tr>';
-  echo '<tr><td>network_address</td><td>'.$json_grp['network_address'].'</td></tr>';
-  echo '<tr><td>suspended_jobs</td><td>'.$json_grp['suspended_jobs'].'</td></tr>';
-  echo '<tr><td>next_finaud_decision</td><td>'.$json_grp['next_finaud_decision'].'</td></tr>';
-  echo '<tr><td>last_available_upto</td><td>'.$json_grp['last_available_upto'].'</td></tr>';
-  echo '<tr><td>mem</td><td>'.$json_grp['mem'].'</td></tr>';
-  echo '<tr><td>state_num</td><td>'.$json_grp['state_num'].'</td></tr>';
-  echo '<tr><td>type</td><td>'.$json_grp['type'].'</td></tr>';
-  echo '<tr><td>next_state</td><td>'.$json_grp['next_state'].'</td></tr>';
-echo '
-  </table>
-  </div><!-- end col-md-6 -->
-</div><!-- end row -->
+  			echo '<tr><td>scheduler_priority</td><td>'.$json_grp['scheduler_priority'].'</td></tr>';
+  			echo '<tr><td>finaud_decision</td><td>'.$json_grp['finaud_decision'].'</td></tr>';
+  			echo '<tr><td>core</td><td>'.$json_grp['core'].'</td></tr>';
+  			echo '<tr><td>deploy</td><td>'.$json_grp['deploy'].'</td></tr>';
+  			echo '<tr><td>besteffort</td><td>'.$json_grp['besteffort'].'</td></tr>';
+  			echo '<tr><td>cpuset</td><td>'.$json_grp['cpuset'].'</td></tr>';
+  			echo '<tr><td>last_job_date</td><td>'.$json_grp['last_job_date'].'</td></tr>';
+  			echo '<tr><td>desktop_computing</td><td>'.$json_grp['desktop_computing'].'</td></tr>';
+  			echo '<tr><td>drain</td><td>'.$json_grp['drain'].'</td></tr>';
+  			echo '<tr><td>state</td><td>'.$json_grp['state'].'</td></tr>';
+  			echo '<tr><td>available_upto</td>';
+  			switch($json_grp['available_upto']){
+  				  case 0:
+  				    echo '<td>'.'0'.'</td></tr>';
+  				    break;
+  				  case 1:
+  				    echo '<td>'.'1'.'</td></tr>';
+  				    break;
+  				  case 2147483647:
+  				    echo '<td>'.'Forever waking-up'.'</td></tr>';
+  				    break;
+  				  case 2147483646:
+  				    echo '<td>'.'Forever'.'</td></tr>';
+  				    break;
+  			}
+  			echo '<tr><td>id</td><td>'.$json_grp['id'].'</td></tr>';
+  			echo '<tr><td>cpu</td><td>'.$json_grp['cpu'].'</td></tr>';
+  			echo '<tr><td>api_timestamp</td><td>'.date('d/m/y',$json_grp['api_timestamp']).'</td></tr>';
+  			echo '<tr><td>expiry_date</td><td>'.$json_grp['expiry_date'].'</td></tr>';
+  			echo '<tr><td>host</td><td>'.$json_grp['host'].'</td></tr>';
+  			echo '<tr><td>network_address</td><td>'.$json_grp['network_address'].'</td></tr>';
+  			echo '<tr><td>suspended_jobs</td><td>'.$json_grp['suspended_jobs'].'</td></tr>';
+  			echo '<tr><td>next_finaud_decision</td><td>'.$json_grp['next_finaud_decision'].'</td></tr>';
+  			echo '<tr><td>last_available_upto</td><td>'.$json_grp['last_available_upto'].'</td></tr>';
+  			echo '<tr><td>mem</td><td>'.$json_grp['mem'].'</td></tr>';
+  			echo '<tr><td>state_num</td><td>'.$json_grp['state_num'].'</td></tr>';
+  			echo '<tr><td>type</td><td>'.$json_grp['type'].'</td></tr>';
+  			echo '<tr><td>next_state</td><td>'.$json_grp['next_state'].'</td></tr>';
+			echo '
+  			</table>
+  		</div><!-- end col-md-6 -->
+	</div><!-- end row -->
 </div><!-- end container theme-showcase -->';
 include 'footer.php';
 }else{
-  header("location:/webui-oardocker/errors.php?pb=nocore");
+
+  header("location:/webui-oardocker/nocore.php");
   exit();
 }
 
